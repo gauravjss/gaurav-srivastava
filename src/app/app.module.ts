17445167508bfb906aac1be2a84d120591853976
Gaurav Srivastava <gauravjss@gmail.com>
2018-03-31 02:20:59 -0500

    Deploy to Heroku

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,25 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TimelineComponent } from './timeline/timeline.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { SkillsComponent } from './skills/skills.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimelineComponent,
+    PortfolioComponent,
+    SkillsComponent,
+    NavigationComponent,
+    ContactComponent,
+    AboutComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
